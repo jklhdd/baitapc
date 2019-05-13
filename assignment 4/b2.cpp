ca3e44df99777f99aec09dbb1c330d1fdfdd9e31
jklhdd <49911153+jklhdd@users.noreply.github.com>
2019-05-13 13:34:23 +0700

    nopbaitap

--- assignment 4/b2.cpp
+++ assignment 4/b2.cpp
@@ -2,7 +2,7 @@
 int main(){
 	int f0,f1,fn;
 	f0=0;f1=1;fn=0;
-	for(int i=2;i<=38;i++){				//100 bi tran so nen em chay den 38 
+	for(int i=2;i<=10;i++){				//100 bi tran so nen em chay den 38 
 		fn=f0+f1;
 		f0=f1;
 		f1=fn;			
